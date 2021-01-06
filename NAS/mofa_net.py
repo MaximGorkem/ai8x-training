Fix in depth sampling to prevent samplin 0 depth for any of the units. sets depth sampling at level 2 when sampling the width parameters

NAS/mofa_net.py
@@ -152,7 +152,7 @@ class MOFA_Net_Arch():
             if level < 0:
                 min_depth = 1
             else:
-                min_depth = param_dict['depth_list'][u_ind] - level
+                min_depth = max(param_dict['depth_list'][u_ind] - level, 1)
         
             max_depth = param_dict['depth_list'][u_ind]
             depth = random.randint(min_depth, max_depth)
@@ -167,13 +167,14 @@ class MOFA_Net_Arch():
         last_layer_width = self.width_list[-1][-1]
 
         if sample_kernel_depth:
-            param_dict = self.sample_depth(sample_kernel=True).get_param_dict()
+            param_dict = self.sample_depth(sample_kernel=True, level=2).get_param_dict()
         else:
             param_dict = copy.deepcopy(self.get_param_dict())
         
         for u_ind in range(param_dict['n_units']):
             for l_ind in range(param_dict['depth_list'][u_ind]):
                 if u_ind == (param_dict['n_units'] - 1) and l_ind == (param_dict['depth_list'][u_ind] - 1):
+                    #print(f'{u_ind}, {l_ind}, Last Layer Width: {last_layer_width}')
                     width = last_layer_width
                 else:
                     max_width = param_dict['width_list'][u_ind][l_ind]
@@ -183,7 +184,7 @@ class MOFA_Net_Arch():
                             k = (1.0 - 0.25*(i+1))
                             if k <= 0:
                                 break
-                            possible_width_list.append(k * max_width)
+                            possible_width_list.append(int(k * max_width))
                     #possible_width_list = [int(.5*max_width), int(.75*max_width), max_width]
                     width = random.choice(possible_width_list)
                 param_dict['width_list'][u_ind][l_ind] = width
@@ -363,5 +364,6 @@ class MOFAnet(nn.Module):
             x = self.max_pool(x)
         x = self.units[-1](x)
         x = x.view(x.size(0), -1)
+        #print(x.shape, self.depth_list)
         x = self.classifier(x)
         return x

NAS/train_mofa_net_kernel_depth_width_modifs.py
@@ -25,7 +25,7 @@ def cross_entropy_loss_with_soft_target(pred, soft_target):
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
 
-mofa_base = torch.load('mofa_models/mofa_ds_acc41%_epoch4999.pth.tar', map_location=device)
+mofa_base = torch.load('mofa_models/mofa_ds_lev2_acc59%.pth.tar', map_location=device)
 mofa_base_arch = mofa_net.MOFA_Net_Arch(mofa_base.get_arch())
 print(mofa_base_arch)
 
@@ -47,13 +47,14 @@ criterion = torch.nn.CrossEntropyLoss()
 
 level = 1
 best_val_accuracy = 0
-max_epochs = 20000
-init_epochs = 10000
+max_epochs = 4000
+init_epochs = 0
 
 optimizer = torch.optim.SGD(mofa_base.parameters(), lr=1e-3, momentum=0.9)
 # max_optim_steps = 50
 # optim_step = 0
 # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, max_optim_steps, eta_min=1e-5)
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10000, 15000], gamma=0.5)
 
 for epoch in range(init_epochs, init_epochs+max_epochs):
     t0 = time.time()
@@ -62,7 +63,7 @@ for epoch in range(init_epochs, init_epochs+max_epochs):
         batch, labels = batch.to(device), labels.to(device)
 
         mofa_base.sort_channels()
-        subnet_width_arch = mofa_base_arch.sample_width()
+        subnet_width_arch = mofa_base_arch.sample_width(level=level)
         mofa_base.update_arch(subnet_width_arch.get_param_dict())
 
         y_pred = mofa_base(batch)
@@ -83,7 +84,7 @@ for epoch in range(init_epochs, init_epochs+max_epochs):
         optimizer.step()
         mofa_base.reset_arch()
     
-    # scheduler.step()
+    scheduler.step()
     # optim_step += 1
     # if optim_step == max_optim_steps:
     #     optimizer = torch.optim.SGD(mofa_base.parameters(), lr=1e-4, momentum=0.9)